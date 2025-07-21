867adad4d3abcbfa26fd40b0fdb9964b5f248ba8
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-07-21 09:38:55 -0600

    Added getting division events and event rankings Created docstring comments for existing functions

--- events/__init__.py
+++ events/__init__.py
@@ -39,7 +39,33 @@ def get_event(event_code: str) -> Event:
 
   return Event(event_data)
 
+def get_division_events(event_code: str) -> list[Event]:
+  """
+  Get all associated divisions for an event given an event code.
+  :param event_code: Event code to find division events from
+  :return: List of division events, empty if none
+  """
+  season = get_config()['season']
+  division_events: list[Event] = []
+
+  event_response = requests.get(f"http://ftc-api.firstinspires.org/v2.0/{season}/events", auth=get_auth())
+  all_event_data = event_response.json()['events']
+
+  division_event_codes = [event['code'] for event in all_event_data if event['divisionCode'] == event_code]
+  for event_code in division_event_codes:
+    division_events.append(get_event(event_code))
+
+  return division_events
+
+
 def get_all_events(region: str = "", min_date: date = date.min, max_date: date = date.max) -> list[Event]:
+  """
+  Get all events within parameters. By default, returns all events.
+  :param region: OPTIONAL, get events from a certain region with given region code
+  :param min_date: OPTIONAL, get events that end after a certain date
+  :param max_date: OPTIONAL, get events that end before a certain date
+  :return: List of all events that fit the given criteria
+  """
   from events.Event import Event
   config = get_config()
 
@@ -71,3 +97,21 @@ def get_all_events(region: str = "", min_date: date = date.min, max_date: date =
   events.sort(key=lambda event: datetime.fromisoformat(event.dateStart).date())
 
   return events
+
+
+def get_event_rankings(event_code: str) -> dict[int, int]:
+  """
+  Get event rankings for each team from a given event code
+  :param event_code: Valid FTC Event Code
+  :return: Dictionary where the key is a team number and the value is the team's rank at the evvent, empty if no ranking
+  """
+  season = get_config()['season']
+  rank_response = requests.get(f"http://ftc-api.firstinspires.org/v2.0/{season}/rankings/{event_code}", auth=get_auth())
+
+  if rank_response.status_code == 404:
+    raise ValueError(f"The event {event_code} you tried to find does not exist.")
+
+  rank_data = rank_response.json()['rankings']
+  rankings = {rank['teamNumber']: rank['rank'] for rank in rank_data}
+
+  return rankings
